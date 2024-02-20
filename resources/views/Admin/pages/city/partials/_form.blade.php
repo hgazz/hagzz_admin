ac2b41f99747c0f353e4817667192eecf8272889
Genius-code <moh.aly8890@gmail.com>
2024-02-20 13:36:43 +0200

    update_route

--- resources/views/Admin/pages/city/partials/_form.blade.php
+++ resources/views/Admin/pages/city/partials/_form.blade.php
@@ -5,7 +5,7 @@
             <div class="col-md-6 mb-3">
                 <label for="{{$name}}" class="form-label">{{$name}}</label>
                 <input type="text" id="{{$name}}" name="{{$name}}" maxlength="50" class="form-control"
-                       value=" @if ($name == 'name_en') {{old($name, $city?->getTranslation('name','en')  ?? '')}} @else {{old($name, $city?->getTranslation('name','ar')  ?? '')}} @endif "
+                       value=" @if ($name == 'name_en') {{old($name, isset($city) ? $city->getTranslation('name','en')  : '')}} @else {{old($name, isset($city) ? $city->getTranslation('name','ar')  : '')}} @endif "
                        placeholder="Enter {{$name}}" data-parsley-required-message="Please enter {{$name}}">
                 @error($name)
                 <span class="text-danger">*{{$message}}</span>
