f6b4f2863ccfd63a58f0edcdd61567e26fed318b
Genius-code <moh.aly8890@gmail.com>
2024-03-09 10:16:33 +0200

    update_logo

--- resources/views/Admin/pages/faqs/partials/_form.blade.php
+++ resources/views/Admin/pages/faqs/partials/_form.blade.php
@@ -5,7 +5,7 @@
             <div class="col-md-6 mb-3">
                 <label for="{{$name}}" class="form-label">{{$name}}</label>
                 <input type="text" id="{{$name}}" name="{{$name}}" maxlength="50" class="form-control"
-                       value=" @if ($name == 'question_en') {{old($name, isset($faq) ? $faq->getTranslation('question','en')  : '')}} @elseif($name == 'answer_en') {{old($name, isset($faq) ? $faq->getTranslation('answer','en')  : '')}} @elseif($name == 'answer_ar') {{old($name, isset($faq) ? $faq->getTranslation('answer','ar')  : '')}} @else {{old($name, isset($faq) ? $faq->getTranslation('question','en')  : '')}} @endif "
+                       value=" @if ($name == 'question_en') {{old($name, isset($faq) ? $faq->getTranslation('question','en')  : '')}} @elseif($name == 'answer_en') {{old($name, isset($faq) ? $faq->getTranslation('answer','en')  : '')}} @elseif($name == 'answer_ar') {{old($name, isset($faq) ? $faq->getTranslation('answer','ar')  : '')}} @else {{old($name, isset($faq) ? $faq->getTranslation('question','ar')  : '')}} @endif "
                        placeholder="Enter {{$name}}" data-parsley-required-message="Please enter {{$name}}">
                 @error($name)
                 <span class="text-danger">*{{$message}}</span>
