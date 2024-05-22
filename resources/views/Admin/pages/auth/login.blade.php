e36ef6454e47a6f08eff9f66eeb2a68d28918954
Genuis-code <moh.aly8890@gmail.com>
2024-05-22 11:37:38 +0300

    update_request

--- resources/views/Admin/pages/auth/login.blade.php
+++ resources/views/Admin/pages/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
     <title>{{ trans('admin.auth.admin') . ' | '   . trans('admin.auth.login') }}</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('assetsAdmin/src/assets/img/favicon.ico') }}"/>
+    <link rel="icon" type="image/x-icon" href="{{ asset('assetsAdmin/logo/Primary.svg') }}"/>
     <link href="{{ asset('assetsAdmin/layouts/vertical-light-menu/css/light/loader.css') }}" rel="stylesheet" type="text/css"/>
     <link href="{{ asset('assetsAdmin/layouts/vertical-light-menu/css/dark/loader.css') }}" rel="stylesheet" type="text/css"/>
     <script src="{{ asset('assetsAdmin/layouts/vertical-light-menu/loader.js') }}"></script>
