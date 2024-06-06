d05b457a45068b8f9bd7480a0538966403fe3506
Genuis-code <moh.aly8890@gmail.com>
2024-06-07 00:01:12 +0300

    updates

--- resources/views/Admin/Layouts/inc/sidebar.blade.php
+++ resources/views/Admin/Layouts/inc/sidebar.blade.php
@@ -203,7 +203,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
                     </div>
                 </a>
-                <ul class="collapse submenu list-unstyled {{ Request::routeIs('admin.report.*')  ? 'true' : 'false' }}" id="report" data-bs-parent="#accordionExample">
+                <ul class="collapse submenu list-unstyled {{ Request::routeIs('admin.report.*')  ? 'show' : '' }}" id="report" data-bs-parent="#accordionExample">
                     <li class="menu {{ Request::routeIs('admin.report.settlement')  ? 'active' : '' }}">
                         <a href="{{ route('admin.report.settlement') }}" aria-expanded="false" class="dropdown-toggle">
                             <div class="">
