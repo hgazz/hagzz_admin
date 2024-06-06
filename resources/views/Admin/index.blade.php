f6fad2b0739e5fee797ee0774fbb3a0759589fc3
gouda <dev.mohamedgouda@gmail.com>
2024-06-06 23:32:45 +0300

    update

--- resources/views/Admin/index.blade.php
+++ resources/views/Admin/index.blade.php
@@ -46,20 +46,6 @@
                     <div class="widget-content">
                         <div class="account-box">
 
-                            <div class="info-box">
-                                <div class="icon">
-                                    <span>
-                                          <img src="{{ asset('assetsAdmin/src/assets/img/money-bag.png') }}"
-                                               alt="money-bag">
-                                     </span>
-                                </div>
-
-                                <div class="balance-info">
-                                    <h6>{{ trans('admin.total_income') }}</h6>
-                                    <p>{{ $totalIncome }}</p>
-                                </div>
-                            </div>
-
 {{--                            <div class="card-bottom-section">--}}
 {{--                                <div><span class="badge badge-light-success">+ 13.6% <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trending-up"><polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline><polyline points="17 6 23 6 23 12"></polyline></svg></span></div>--}}
 {{--                                <a href="javascript:void(0);" class="">View Report</a>--}}
