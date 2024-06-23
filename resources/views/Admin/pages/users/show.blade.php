606d62ba10451d01cea9723a0f199bb393f36c9e
Genuis-code <moh.aly8890@gmail.com>
2024-06-23 10:40:42 +0300

    update_show_user_details

--- resources/views/Admin/pages/users/show.blade.php
+++ resources/views/Admin/pages/users/show.blade.php
@@ -25,7 +25,8 @@
                             <nav class="breadcrumb-style-one" aria-label="breadcrumb">
                                 <ol class="breadcrumb">
                                     <li class="breadcrumb-item"><a href="{{ route('admin.index') }}">{{ trans('admin.dashboard') }}</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">{{ trans('admin.user.user') }}</li>
+                                    <li class="breadcrumb-item"><a href="{{ route('admin.user.index') }}">{{ trans('admin.user.user') }}</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">{{ trans('admin.user.show_details') }}</li>
                                 </ol>
                             </nav>
 
@@ -35,48 +36,54 @@
             </div>
         </div>
         <!--  END BREADCRUMBS  -->
+        <div class="row layout-top-spacing">
+            <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
+                <div class="card">
+                    <div class="container mt-4">
+                        <h2 class="text-center"> {{trans('admin.user.Trainings')}} </h2>
+                        <div class="row mt-4">
+                            @foreach($showUser->joins as $join)
+                                <div class="col-md-4">
+                                    <div class="card" style="width: 18rem;">
+                                        <img src="{{$join->training->sport->icon}}" class="card-img-top" alt="...">
+                                        <div class="card-body">
+                                            <h5 class="card-title">{{$join->training->name}}</h5>
+                                            <p class="card-text">{{$join->training->description}}</p>
+                                            <span class="text-success">{{$join->training->start_date}}</span>
+                                            /
+                                            <span class="text-danger">{{$join->training->end_date}}</span>
+                                            <br>
+                                            <span>{{trans('admin.user.coach')}} :{{$join->training->coach->name}} </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                        <hr>
+                        <h2 class="text-center"> {{trans('admin.user.Sports')}} </h2>
+                        <div class="row mt-4">
+                            @foreach($showUser->sports as $sport)
+                                <div class="col-md-4">
+                                    <div class="card" style="width: 18rem;">
+                                        <img src="{{$sport->icon}}" class="card-img-top" alt="...">
+                                        <div class="card-body">
+                                            <h5 class="card-title">{{$sport->name}}</h5>
+                                            @if($sport->status == 'active')
+                                                <p class="card-text text-success">{{$sport->status}}</p>
+                                            @else
+                                                <p class="card-text text-danger">{{$sport->status}}</p>
+                                            @endif
+                                            <span class="">{{ trans('admin.academies.academies') }} - {{$sport->academy->commercial_name ?? 'Not Academy'}}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-       <div class="container mt-4">
-           <h2 class="text-center"> {{trans('admin.user.Trainings')}} </h2>
-           <div class="row mt-4">
-               @foreach($showUser->joins as $join)
-                   <div class="col-md-4">
-                       <div class="card" style="width: 18rem;">
-                           <img src="{{$join->training->sport->icon}}" class="card-img-top" alt="...">
-                           <div class="card-body">
-                               <h5 class="card-title">{{$join->training->name}}</h5>
-                               <p class="card-text">{{$join->training->description}}</p>
-                               <span class="text-success">{{$join->training->start_date}}</span>
-                                /
-                               <span class="text-danger">{{$join->training->end_date}}</span>
-                               <br>
-                               <span>{{trans('admin.user.coach')}} :{{$join->training->coach->name}} </span>
-                           </div>
-                       </div>
-                   </div>
-               @endforeach
-           </div>
-           <hr>
-           <h2 class="text-center"> {{trans('admin.user.Sports')}} </h2>
-           <div class="row mt-4">
-               @foreach($showUser->sports as $sport)
-                   <div class="col-md-4">
-                       <div class="card" style="width: 18rem;">
-                           <img src="{{$sport->icon}}" class="card-img-top" alt="...">
-                           <div class="card-body">
-                               <h5 class="card-title">{{$sport->name}}</h5>
-                               @if($sport->status == 'active')
-                                   <p class="card-text text-success">{{$sport->status}}</p>
-                               @else
-                                   <p class="card-text text-danger">{{$sport->status}}</p>
-                               @endif
-                               <span class="">{{ trans('admin.academies.academies') }} - {{$sport->academy->commercial_name ?? 'Not Academy'}}</span>
-                           </div>
-                       </div>
-                   </div>
-               @endforeach
-           </div>
-       </div>
     </div>
 @endsection
 
