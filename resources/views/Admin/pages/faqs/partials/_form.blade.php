4b41888a279aa145dfab7030b8c7763d14f2db46
ali <ali@example.com>
2024-07-15 13:50:14 +0300

    update-language

--- resources/views/Admin/pages/faqs/partials/_form.blade.php
+++ resources/views/Admin/pages/faqs/partials/_form.blade.php
@@ -1,33 +1,31 @@
 @csrf
 <div class="row">
     @foreach (\App\Services\TranslatableService::getTranslatableInputs(App\Models\Faq::class) as $name => $data)
-            <div class="col-md-6 mb-3">
-                <label for="{{$name}}" class="form-label">{{trans('admin.faq.'.$name)}}<code>*</code></label>
-                <input type="text" id="{{$name}}" name="{{$name}}" maxlength="250" class="form-control"
-                       @php
-                           $language = $name == 'question_en' ? 'en' : 'ar';
-                           $defaultValue = isset($faq) ? $faq->getTranslation('question', $language) : '';
-                       @endphp
-                       value="{{ old($name, $defaultValue) }}"
-                       placeholder="Enter {{$name}}" data-parsley-required-message="Please enter {{$name}}">
-                @error($name)
-                <span class="text-danger">*{{$message}}</span>
-                @enderror
-            </div>
-        @endforeach
-        <div class="col-6">
-            <label for="answer_en">{{trans('admin.faq.answer_en')}}<code>*</code></label>
-            <textarea id="answer_en" name="answer_en" class="form-control">{{ old('answer_en', isset($faq) ? $faq->getTranslation('answer', 'en') : '') }}</textarea>
-            @error('answer_en')
-            <span class="text-danger">*{{$message}}</span>
+        <div class="col-md-6 mb-3">
+            <label for="{{ $name }}" class="form-label">{{ trans('admin.faq.' . $name) }}<code>*</code></label>
+            <input type="text" id="{{ $name }}" name="{{ $name }}" maxlength="250" class="form-control"
+                @php
+$language = $name == 'question_en' ? 'en' : 'ar';
+                           $defaultValue = isset($faq) ? $faq->getTranslation('question', $language) : ''; @endphp
+                value="{{ old($name, $defaultValue) }}" placeholder="Enter {{ $name }}"
+                data-parsley-required-message="Please enter {{ $name }}">
+            @error($name)
+                <span class="text-danger">*{{ $message }}</span>
             @enderror
         </div>
-        <div class="col-6">
-            <label for="answer_ar">{{trans('admin.faq.answer_ar')}}<code>*</code></label>
-            <textarea id="answer_ar" name="answer_ar" class="form-control">{{ old('answer_ar', isset($faq) ? $faq->getTranslation('answer', 'ar') : '') }}</textarea>
-            @error('answer_ar')
-            <span class="text-danger">*{{$message}}</span>
-            @enderror
-        </div>
+    @endforeach
+    <div class="col-6">
+        <label for="answer_en">{{ trans('admin.faq.answer_en') }}<code>*</code></label>
+        <textarea id="answer_en" name="answer_en" class="form-control">{{ old('answer_en', isset($faq) ? $faq->getTranslation('answer', 'en') : '') }}</textarea>
+        @error('answer_en')
+            <span class="text-danger">*{{ $message }}</span>
+        @enderror
+    </div>
+    <div class="col-6">
+        <label for="answer_ar">{{ trans('admin.faq.answer_ar') }}<code>*</code></label>
+        <textarea id="answer_ar" name="answer_ar" class="form-control">{{ old('answer_ar', isset($faq) ? $faq->getTranslation('answer', 'ar') : '') }}</textarea>
+        @error('answer_ar')
+            <span class="text-danger">*{{ $message }}</span>
+        @enderror
+    </div>
 </div>
-
