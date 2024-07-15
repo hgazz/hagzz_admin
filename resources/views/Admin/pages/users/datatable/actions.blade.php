1fb0b0f3d9306acdb714bf217b1d1a5f84ceab4a
Genuis-code <moh.aly8890@gmail.com>
2024-07-15 16:12:08 +0300

    update_index

--- resources/views/Admin/pages/users/datatable/actions.blade.php
+++ resources/views/Admin/pages/users/datatable/actions.blade.php
@@ -1,6 +1,6 @@
 <div class="btn-sm btn-group" role="group" aria-label="Button group with nested dropdown">
     <div class="btn-group dropstart" role="group">
-        <button id="btnGroupDrop1" type="button" class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+        <button id="btnGroupDrop1" type="button" class="btn btn-dark dropdown-toggle d-flex align-items-center justify-content-between" data-bs-toggle="dropdown" aria-expanded="false">
             {{ trans('admin.actions') }}
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
         </button>
