4b0785d670896dc794b068c305ba5d413903bcab
Genuis-code <moh.aly8890@gmail.com>
2024-07-20 13:43:41 +0300

    show_training_details

--- resources/views/Admin/pages/training/datatable/actions.blade.php
+++ resources/views/Admin/pages/training/datatable/actions.blade.php
@@ -15,33 +15,33 @@
                 id="updateStatus-{{ $training->id }}">
                 @csrf @method('PUT')
             </form>
-            <li class="mb-1">
-                @if ($training->active)
-                    <a href="javascript:void(0)"
-                        onclick="document.getElementById('updateStatus-{{ $training->id }}').submit();" type="submit"
-                        class="text-success text-center fw-bold" title="{{ $training->active }}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                            class="bi bi-x-square-fill m-2 fw-bold" viewBox="0 0 16 16">
-                            <path
-                                d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708" />
-                        </svg>
-                        {{ trans('admin.academies.make_inactive') }}
-                    </a>
-                @else
-                    <a href="javascript:void(0)"
-                        onclick="document.getElementById('updateStatus-{{ $training->id }}').submit();" type="submit"
-                        class="text-danger text-center fw-bold" title="{{ $training->active }}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                            class="bi bi-check2-square m-2 fw-bold" viewBox="0 0 16 16">
-                            <path
-                                d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5z" />
-                            <path
-                                d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0" />
-                        </svg>
-                        {{ trans('admin.academies.make_active') }}
-                    </a>
-                @endif
-            </li>
+{{--            <li class="mb-1">--}}
+{{--                @if ($training->active)--}}
+{{--                    <a href="javascript:void(0)"--}}
+{{--                        onclick="document.getElementById('updateStatus-{{ $training->id }}').submit();" type="submit"--}}
+{{--                        class="text-success text-center fw-bold" title="{{ $training->active }}">--}}
+{{--                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"--}}
+{{--                            class="bi bi-x-square-fill m-2 fw-bold" viewBox="0 0 16 16">--}}
+{{--                            <path--}}
+{{--                                d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708" />--}}
+{{--                        </svg>--}}
+{{--                        {{ trans('admin.academies.make_inactive') }}--}}
+{{--                    </a>--}}
+{{--                @else--}}
+{{--                    <a href="javascript:void(0)"--}}
+{{--                        onclick="document.getElementById('updateStatus-{{ $training->id }}').submit();" type="submit"--}}
+{{--                        class="text-danger text-center fw-bold" title="{{ $training->active }}">--}}
+{{--                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"--}}
+{{--                            class="bi bi-check2-square m-2 fw-bold" viewBox="0 0 16 16">--}}
+{{--                            <path--}}
+{{--                                d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5z" />--}}
+{{--                            <path--}}
+{{--                                d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0" />--}}
+{{--                        </svg>--}}
+{{--                        {{ trans('admin.academies.make_active') }}--}}
+{{--                    </a>--}}
+{{--                @endif--}}
+{{--            </li>--}}
             <li class="mb-1">
                 <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
                 <a href="{{ route('admin.training.createBooking', $training) }}" class="text-success text-center"
