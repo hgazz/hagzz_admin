dc7f01b54b6bf74a1f85bb9b88026ee5d6df3248
Genuis-code <moh.aly8890@gmail.com>
2024-08-17 09:04:36 +0300

    update_area

--- public/assetsAdmin/yajaraLog.js
+++ public/assetsAdmin/yajaraLog.js
@@ -5,7 +5,7 @@ $(document).ajaxError(function(event, jqxhr, settings, exception) {
         // Prompt user if they'd like to be redirected to the login page
         bootbox.confirm("Your session has expired. Would you like to be redirected to the login page?", function(result) {
             if (result) {
-                window.location = '/login';
+                window.location = '/admin/login';
             }
         });
 
