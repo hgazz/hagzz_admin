197422a1fe0aba4e67d9452a344bab80b499c63a
Genuis-code <moh.aly8890@gmail.com>
2024-09-09 18:13:38 +0300

    fix_bugs

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -14,79 +14,79 @@ class SettingSeeder extends Seeder
     public function run(): void
     {
         DB::table('settings')->truncate();
-$data = [
-    [
-        'key' => 'egypt_address',
-        'value' => 'egypt',
-        'type' => 'text'
-    ],
-    [
-        'key' => 'qatar_address',
-        'value' => 'We are currently under maintenance. Please check back later.',
-        'type' => 'text'
-    ],
-    [
-        'key' => 'phone',
-        'value' => '123456789',
-        'type' => 'text'
-    ],
-    [
-        'key' => 'whatsapp',
-        'value' => '123456789',
-        'type' => 'text'
-    ],
-    [
-        'key' => 'email',
-        'value' => 'mail@mail.com',
-        'type' => 'text'
-    ],
-    [
-        'key' => 'facebook',
-        'value' => 'https://facebook.com/bokitapp',
-        'type' => 'text'
-    ],
-    [
-        'key' => 'twitter',
-        'value' => 'https://twitter.com/bokitapp',
-        'type' => 'text'
-    ],
-    [
-        'key' => 'instagram',
-        'value' => 'https://instagram.com/bokitapp',
-        'type' => 'text'
-    ],
-    [
-        'key' => 'snapchat',
-        'value' => 'https://snapchat.com/bokitapp',
-        'type' => 'text'
-    ],
-    [
-        'key' => 'telegram',
-        'value' => 'https://telegram.com/bokitapp',
-        'type' => 'text'
-    ],
-    [
-        'key' => 'terms',
-        'value' => 'data',
-        'type' => 'textarea'
-    ],
-    [
-        'key' => 'youtube',
-        'value' => 'https://youtube.com/bokitapp',
-        'type' => 'text'
-    ],
-    [
-        'key' => 'logo',
-        'value' => 'https://bokitapp.com/logo.png',
-        'type' => 'image'
-    ],
-    [
-        'key' => 'privacy',
-        'value' => 'data',
-        'type' => 'textarea',
-    ],
-];
+        $data = [
+            [
+                'key' => 'egypt_address',
+                'value' => 'egypt',
+                'type' => 'text'
+            ],
+            [
+                'key' => 'qatar_address',
+                'value' => 'We are currently under maintenance. Please check back later.',
+                'type' => 'text'
+            ],
+            [
+                'key' => 'phone',
+                'value' => '123456789',
+                'type' => 'text'
+            ],
+            [
+                'key' => 'whatsapp',
+                'value' => '123456789',
+                'type' => 'text'
+            ],
+            [
+                'key' => 'email',
+                'value' => 'mail@mail.com',
+                'type' => 'text'
+            ],
+            [
+                'key' => 'facebook',
+                'value' => 'https://facebook.com/bokitapp',
+                'type' => 'text'
+            ],
+            [
+                'key' => 'twitter',
+                'value' => 'https://twitter.com/bokitapp',
+                'type' => 'text'
+            ],
+            [
+                'key' => 'instagram',
+                'value' => 'https://instagram.com/bokitapp',
+                'type' => 'text'
+            ],
+            [
+                'key' => 'snapchat',
+                'value' => 'https://snapchat.com/bokitapp',
+                'type' => 'text'
+            ],
+            [
+                'key' => 'telegram',
+                'value' => 'https://telegram.com/bokitapp',
+                'type' => 'text'
+            ],
+            [
+                'key' => 'terms',
+                'value' => 'data',
+                'type' => 'textarea'
+            ],
+            [
+                'key' => 'youtube',
+                'value' => 'https://youtube.com/bokitapp',
+                'type' => 'text'
+            ],
+            [
+                'key' => 'logo',
+                'value' => 'https://bokitapp.com/logo.png',
+                'type' => 'image'
+            ],
+            [
+                'key' => 'privacy',
+                'value' => 'data',
+                'type' => 'textarea',
+            ],
+        ];
 
-DB::table('settings')->insert($data);
+        DB::table('settings')->insert($data);
     }
 }
