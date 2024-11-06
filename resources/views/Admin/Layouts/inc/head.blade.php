e6338097da0f5803bef1b79b0bc2c4cf57c7ba03
Ali Talaat <alitalaatali30@gmail.com>
2024-11-06 13:40:01 +0200

    update

--- resources/views/Admin/Layouts/inc/head.blade.php
+++ resources/views/Admin/Layouts/inc/head.blade.php
@@ -5,24 +5,26 @@
 
 <!-- BEGIN GLOBAL MANDATORY STYLES -->
 @if (app()->getLocale() == 'ar')
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap" rel="stylesheet">
-    <link href="{{ asset('assetsAdmin/ar/src/bootstrap/css/bootstrap.rtl.min.css') }}" rel="stylesheet"
-        type="text/css" />
-    <link href="{{ asset('assetsAdmin/ar/layouts/vertical-light-menu/css/light/plugins.css') }}" rel="stylesheet"
-        type="text/css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap"
-        rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap" rel="stylesheet">
+<link href="{{ asset('assetsAdmin/ar/src/bootstrap/css/bootstrap.rtl.min.css') }}" rel="stylesheet"
+    type="text/css" />
+<link href="{{ asset('assetsAdmin/ar/layouts/vertical-light-menu/css/light/plugins.css') }}" rel="stylesheet"
+    type="text/css" />
+<link href="{{ asset('assetsAdmin/ar/layouts/vertical-light-menu/css/dark/plugins.css') }}" rel="stylesheet"
+    type="text/css" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap"
+    rel="stylesheet">
 @else
-    <link href="https://fonts.googleapis.com/css?family=Cairo:400,600,700" rel="stylesheet">
-    <link href="{{ asset('assetsAdmin/src/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('assetsAdmin/layouts/vertical-light-menu/css/light/plugins.css') }}" rel="stylesheet"
-        type="text/css" />
-    <link href="{{ asset('assetsAdmin/layouts/vertical-light-menu/css/dark/plugins.css') }}" rel="stylesheet"
-        type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=Cairo:400,600,700" rel="stylesheet">
+<link href="{{ asset('assetsAdmin/src/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assetsAdmin/layouts/vertical-light-menu/css/light/plugins.css') }}" rel="stylesheet"
+    type="text/css" />
+<link href="{{ asset('assetsAdmin/layouts/vertical-light-menu/css/dark/plugins.css') }}" rel="stylesheet"
+    type="text/css" />
 @endif
 <!-- END GLOBAL MANDATORY STYLES -->
 @stack('css')
