8ba3737c66ef67825c58fd50e74beb4890c159b1
Genuis-code <moh.aly8890@gmail.com>
2024-11-11 19:16:31 +0200

    update

--- public/assetsAdmin/yajaraLog.js
+++ public/assetsAdmin/yajaraLog.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-    if (typeof $.fn.dataTable !== 'undefined') {
-        $.fn.dataTable.ext.errMode = 'none';
-    }
+    // if (typeof $.fn.dataTable != 'undefined') {
+    //     $.fn.dataTable.ext.errMode = 'none';
+    // }
 
     $(document).ajaxError(function(event, jqxhr, settings, exception) {
         if (exception === 'Unauthorized') {
             bootbox.confirm("Your session has expired. Would you like to be redirected to the login page?", function(result) {
                 if (result) {
-                    window.location.href = 'https://admin.hagzz.com/admin/admin';
+                    window.location.href = 'https://admin.hagzz.com/admin/';
                 }
             });
         }
