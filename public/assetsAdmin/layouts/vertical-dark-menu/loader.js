bf01e603fd5112949a9290cd1db6385cde5d86ee
Ali Talaat <alitalaatali30@gmail.com>
2024-11-11 10:49:28 +0200

    update

--- public/assetsAdmin/layouts/vertical-dark-menu/loader.js
+++ public/assetsAdmin/layouts/vertical-dark-menu/loader.js
@@ -37,7 +37,7 @@ window.addEventListener("load", function(){
         ParsedObject = getParseObject;
 
         if (getcorkThemeObject !== null) {
-               
+
             if (ParsedObject.admin === 'Cork Admin Template') {
 
                 if (ParsedObject.settings.layout.name === layoutName) {
@@ -46,7 +46,7 @@ window.addEventListener("load", function(){
                 } else {
                     corkThemeObject = settingsObject;
                 }
-                
+
             } else {
                 if (ParsedObject.admin === undefined) {
                     corkThemeObject = settingsObject;
@@ -59,24 +59,16 @@ window.addEventListener("load", function(){
     }
 
     // Get Dark Mode Information i.e darkMode: true or false
-    
+
     if (corkThemeObject.settings.layout.darkMode) {
         localStorage.setItem("theme", JSON.stringify(corkThemeObject));
         getcorkThemeObject = localStorage.getItem("theme");
         getParseObject = JSON.parse(getcorkThemeObject)
-    
+
         if (getParseObject.settings.layout.darkMode) {
             ifStarterKit = document.body.getAttribute('page') === 'starter-pack' ? true : false;
             document.body.classList.add('dark');
-            if (ifStarterKit) {
-                if (document.querySelector('.navbar-logo')) {
-                    document.querySelector('.navbar-logo').setAttribute('src', '../../src/assets/img/logo.svg')
-                }
-            } else {
-                if (document.querySelector('.navbar-logo')) {
-                    document.querySelector('.navbar-logo').setAttribute('src', getParseObject.settings.layout.logo.darkLogo)
-                }
-            }
+
         }
     } else {
         localStorage.setItem("theme", JSON.stringify(corkThemeObject));
@@ -86,29 +78,21 @@ window.addEventListener("load", function(){
         if (!getParseObject.settings.layout.darkMode) {
             ifStarterKit = document.body.getAttribute('page') === 'starter-pack' ? true : false;
             document.body.classList.remove('dark');
-            if (ifStarterKit) {
-                if (document.querySelector('.navbar-logo')) {
-                    document.querySelector('.navbar-logo').setAttribute('src', '../../src/assets/img/logo2.svg')
-                }
-            } else {
-                if (document.querySelector('.navbar-logo')) {
-                    document.querySelector('.navbar-logo').setAttribute('src', getParseObject.settings.layout.logo.lightLogo)
-                }
-            }
-            
+
+
         }
     }
 
     // Get Layout Information i.e boxed: true or false
 
     if (corkThemeObject.settings.layout.boxed) {
-    
+
         localStorage.setItem("theme", JSON.stringify(corkThemeObject));
         getcorkThemeObject = localStorage.getItem("theme");
         getParseObject = JSON.parse(getcorkThemeObject)
-    
+
         if (getParseObject.settings.layout.boxed) {
-            
+
             if (document.body.getAttribute('layout') !== 'full-width') {
                 document.body.classList.add('layout-boxed');
                 if (document.querySelector('.header-container')) {
@@ -126,15 +110,15 @@ window.addEventListener("load", function(){
                     document.querySelector('.middle-content').classList.remove('container-xxl');
                 }
             }
-            
+
         }
-        
+
     } else {
-        
+
         localStorage.setItem("theme", JSON.stringify(corkThemeObject));
         getcorkThemeObject = localStorage.getItem("theme");
         getParseObject = JSON.parse(getcorkThemeObject)
-        
+
         if (!getParseObject.settings.layout.boxed) {
 
             if (document.body.getAttribute('layout') !== 'boxed') {
@@ -157,9 +141,9 @@ window.addEventListener("load", function(){
         }
     }
 
-    
 
 
-    
+
+
 });
 
